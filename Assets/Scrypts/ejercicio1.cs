60c9c7f9468d0874a9026e936ed9acb2759e8953
LaPietraFranco <101827023+LaPietraFranco@users.noreply.github.com>
2022-04-21 15:48:50 -0300

    exercise1 completed

---
+++ Assets/Scrypts/ejercicio1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Realizar un rpograma que muestre los numeros de forma descendente entre dos grupos enteros ingresados por el usuario, incluyendolos. 
+// Chequear que no sean iguales y que el primero sea mayor que el segundo.
+
+
+
+public class ejercicio1 : MonoBehaviour
+{
+    public int n1;
+    public int n2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (n1 == n2)
+        {
+            Debug.Log("Los numeros no pueen ser iguales");
+        }
+
+        else if (n1 < n2)
+        {
+            Debug.Log("El segundo numero no puede ser menor que el primero");
+        }
+        else
+        {
+            while (n1 >= n2)
+          {
+           Debug.Log(n1);
+           n1--;
+          }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
